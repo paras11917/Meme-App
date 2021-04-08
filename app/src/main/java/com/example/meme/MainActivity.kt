3681900fe5166cf74c40693ca604861d3a4c7589
paras <paras11917@gmail.com>
2021-04-08 20:31:53 +0530

    MemeApp created by Paras Pipre

---
+++ app/src/main/java/com/example/meme/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.meme
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class MainActivity : AppCompatActivity() {
+
+    var currentImageUrl: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadmeme()
+    }
+
+    private fun loadmeme() {
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        // Instantiate the RequestQueue.
+        progressBar.visibility = View.VISIBLE
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener{ response ->
+                currentImageUrl = response.getString("url")
+                Glide.with(this).load(currentImageUrl).listener(object:RequestListener<Drawable> {
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                        TODO("Not yet implemented")
+                    }
+
+                } ).into(imageView)
+
+
+            },
+            Response.ErrorListener {
+                Toast.makeText(this,"Something went wrong",Toast.LENGTH_LONG).show()
+
+            })
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+
+
+
+
+
+    fun nextmeme(view: View) {
+        loadmeme()
+    }
+    fun sharememe(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey,Checkout this cool meme I get from redit $currentImageUrl ")
+        val chooser = Intent.createChooser(intent, "Share this meme using...")
+        startActivity(chooser)
+    }
+}
